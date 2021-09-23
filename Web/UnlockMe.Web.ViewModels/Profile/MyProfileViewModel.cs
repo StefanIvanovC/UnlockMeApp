36dfacc24fc02d83c099dd8a3e94bf77eea523b4
Stefan <stefanivanovc@gmail.com>
2021-09-23 18:13:28 +0300

    Add ProfilesController implement viewModel

--- Web/UnlockMe.Web.ViewModels/Profile/MyProfileViewModel.cs
+++ Web/UnlockMe.Web.ViewModels/Profile/MyProfileViewModel.cs
@@ -1,9 +1,10 @@
 ﻿namespace UnlockMe.Web.ViewModels.Profile
 {
     using System;
-
+    using System.Collections.Generic;
     using UnlockMe.Data.Models;
     using UnlockMe.Services.Mapping;
+    using UnlockMe.Web.ViewModels.Post;
 
     public class MyProfileViewModel : IMapFrom<ApplicationUser>
     {
@@ -11,8 +12,16 @@
 
         public string ProfileImagePath { get; set; }
 
+        public string UserName { get; set; }
+
         public string AboutMe { get; set; }
 
         public DateTime CreatedOn { get; set; }
+
+        public int PostsCount { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<SinglePostViewModel> Posts { get; set; }
     }
 }
--- Web/UnlockMe.Web/Controllers/ProfilesController.cs
+++ Web/UnlockMe.Web/Controllers/ProfilesController.cs
@@ -3,26 +3,42 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using UnlockMe.Data;
     using UnlockMe.Data.Models;
     using UnlockMe.Services.Data;
+    using UnlockMe.Web.ViewModels.Post;
+    using UnlockMe.Web.ViewModels.Profile;
 
     [Authorize]
     public class ProfilesController : Controller
     {
-        private readonly IProfileService profileService;
+        private readonly ApplicationDbContext db;
         private readonly UserManager<ApplicationUser> userManager;
 
         public ProfilesController(
-            IProfileService profileService,
+            ApplicationDbContext db,
             UserManager<ApplicationUser> userManager)
         {
-            this.profileService = profileService;
+            this.db = db;
             this.userManager = userManager;
         }
 
-        public IActionResult MyProfile()
+        public async Task<IActionResult> MyProfile()
         {
-            return this.Ok();
+            var user = await this.userManager.GetUserAsync(this.User);
+            var posts = this.db.Posts.Where(p => p.AddedByUserId == user.Id);
+
+            var viewModel = new MyProfileViewModel
+            {
+                UserName = user.UserName,
+                AboutMe = user.AboutMe,
+                CreatedOn = user.CreatedOn,
+                PostsCount = posts.Count(),
+            };
+
+            return this.View(viewModel);
         }
     }
 }
